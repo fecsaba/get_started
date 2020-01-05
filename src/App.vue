7e5510693ef061a7c207b5bb842f3dc90a4318a0
fecsaba <nexusegyesulet@gmail.com>
2020-01-05 17:03:26 +0100

    Two way data binding

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,24 @@
 <template>
   <div id="app">
-    <h1 v-once>{{ title }}</h1>
-    <input type="text" v-bind:placeholder="ph" @input="changeTitle">
-    <p>{{ title }}</p>
-    <p>{{ sayHello() }} - <a :href="link">{{google}}</a></p>
-    <hr>
-    <p>{{ finishedLink }}</p>
-    <p v-html="finishedLink"> </p>
-    <hr>
-    <button v-on:click="increase(2, $event)">Click me</button>
-    <button v-on:click="counter++">Click me v2</button>
-    <h3>{{ counter }}</h3>
-    <p v-on:mousemove="updateCoordinates">
-      Coordinates: {{ x }} / {{ y }} - <span v-on:mousemove.stop="">DEAD-SPOT</span></p>
-    <hr>
-    <input type="text" v-on:keyup.enter="alertMe"/>
-    </div>
+    <input type="text" v-model="name"/>
+    <p>{{ name }}</p>
+
+  </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'app',
-    data() {
-      return {
-          title: 'Hello',
-        ph: 'Write here any',
-        link: 'https://google.com',
-        google: 'Google',
-        finishedLink: '<a href="http://google.com">Google</a>',
-        counter: 0,
-        x: 0,
-        y: 0
+    data: function() {
+    return {
+        name: 'Rob'
       }
     },
     methods: {
 
-        changeTitle: function (event) {
-            this.title = event.target.value
-        },
-      sayHello: function () {
-          this.title = 'Hi'
-        return this.title + ' vue'
-      },
-      increase: function(step, event) {
-          this.counter += step
-        // eslint-disable-next-line no-console
-        console.log(event)
-      },
-      updateCoordinates: function(event) {
-          this.x = event.clientX
-          this.y = event.clientY
-      },
-      dummy: function(event) {
-          event.stopPropagation()
-      },
-      alertMe: function() {
-          alert('Alert')
-      }
     }
-
-
 }
 
 </script>
